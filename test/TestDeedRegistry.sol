0462c842b5779463f91e3cdea9b2338b2510c15d
Rian Finnegan <flanagan89@gmail.com>
2018-02-09 11:41:37 +1100

    formatting
    didn't build or test this... hope it works!

--- test/TestDeedRegistry.sol
+++ test/TestDeedRegistry.sol
@@ -1,61 +1,55 @@
-pragma solidity ^0.4.2;
+pragma solidity ^0.4.18;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/DeedRegistry.sol";
 
-contract TestDeedRegistry 
-{
+contract TestDeedRegistry {
 	string constant ownerName = "Yuri Gagarin";
 	int64 constant ownerDOB = -1130241600;
 	string constant fileName = "HelloWorld.txt";	
 	uint constant fileSize = 12;
 	string constant fileDescription = "A hello world document.";
-	
-	address testChangeAddress = 0xf17f52151EbEF6C7334FAD080c5704D77216b732;
 
-	function fakeHash() private pure returns (byte[256])
-	{
+	function fakeHash() private pure returns (byte[256]) {
 		byte[256] memory fakeChecksum;
-        for(uint i = 0; i < 256; ++i)
-        {
-            if(i%0 ==0)
-            {
+        for (uint i = 0; i < 256; ++i) {
+            if (i%0 == 0) {
                 fakeChecksum[i] = 1;
             }
         }
 		return fakeChecksum;
 	}
 
-    function testAdd() public 
-    {
-        var registry = DeedRegistry(DeployedAddresses.DeedRegistry());
-        assert(registry.createDeed(msg.sender, 
+    function testAdd() public {
+        var registry = new DeedRegistry();
+		bool result = registry.createDeed(msg.sender, 
 			ownerName, 
 			ownerDOB, 
 			fileName, 
 			fileSize, 
 			fileDescription, 
-			fakeHash()));
+			fakeHash());
+        Assert.isTrue(result, "new deed should be created");
     }
 	
-	function testChangeRegistrar() public
-	{
-		var registry = DeedRegistry(DeployedAddresses.DeedRegistry());
+	function testChangeRegistrar() public {
+		address testChangeAddress = 0xf17f52151EbEF6C7334FAD080c5704D77216b732;
+		var registry = new DeedRegistry();
 		registry.addRegistrar(testChangeAddress);
-		assert(registry.isRegistrar(testChangeAddress));
+		bool result = registry.isRegistrar(testChangeAddress);
+		Assert.isTrue(result, "added address should be a registrar");
 	}
 	
-	function testRemoveRegistrar() public
-	{
-		var registry = DeedRegistry(DeployedAddresses.DeedRegistry());
+	function testRemoveRegistrar() public {
+		address testChangeAddress = 0xf17f52151EbEF6C7334FAD080c5704D77216b732;
+		var registry = new DeedRegistry();
 		registry.removeRegistrar(testChangeAddress);
-		assert(!registry.isRegistrar(testChangeAddress));
+		Assert.isFalse(registry.isRegistrar(testChangeAddress), "removed address should not be a registrar");
 	}
 	
-	function testCheckDeedExists() public
-	{
-		var registry = DeedRegistry(DeployedAddresses.DeedRegistry());
-		assert(registry.deedExists(fakeHash()));
+	function testCheckDeedExists() public {
+		var registry = new DeedRegistry();
+		Assert.isTrue(registry.deedExists(fakeHash()), "deed should exist");
 	}
 }
